2267f847d894bde411b1c6aae5e48d7600422eaf
DejanAcimovic <dacimovic1@etf.unsa.ba>
2018-04-28 23:49:32 +0200

    Inicijalna postavka

---
+++ formsapp/src/Components/LoginForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+
+class LoginForm extends Component {
+  render() {
+    return (
+      <p>Ovo ce biti nasa login forma</p>
+    );
+  }
+}
+
+export default LoginForm;
